35379e4220bb03171061b618403a9fad09fa9509
mohsin ali <110420390+ma2205505@users.noreply.github.com>
2022-09-16 20:21:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+//===============destructing in Array===========
+
+// let array = ["abc","xyz"];
+// let [o,p] = array;
+// console.log(o); 
+// console.log(p); 
+
+
+
+//===============destructing in object===========
+
+// let obj = {
+//     name: "mohsin",
+//     age: 24,
+//     classp: "intermidiate",
+// };
+// let {name , classp} = obj;
+// console.log(name); 
+// console.log(classp);
+
+
+
+
+
+// let arr = ['mohsin','ali','03458855','ma2205505@gmail.com','karachi']
+// let {name, lastname, number, gmail , location} = arr
+// let myobj = {
+//     name:name,
+//     lastname:lastname,
+//     number:number,
+//     gmail:gmail,
+//     location:location,
+// }
+// console.log(myobj)
+
+
+       //=========assync function============
+
+    // async function asyncCall(){
+    //     console.log('calling');
+    //     console.log('calling');
+    // }
+
+
+    
+
